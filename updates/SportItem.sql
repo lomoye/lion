d291186ec65665a8dbd3b3f2b377cf3eb303fab1
lomoye <834033206@qq.com>
2018-01-29 20:52:43 +0800

    fixed

--- updates/SportItem.sql
+++ updates/SportItem.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS sport_item;
 CREATE TABLE sport_item(
 id bigint(20) NOT NULL AUTO_INCREMENT,
-user_id bigint(20),
-name varchar(128),
+user_id bigint(20) NOT NULL,
+name varchar(128) NOT NULL,
 create_time datetime NOT NULL,
 modify_time datetime NOT NULL,
 PRIMARY KEY (id) 
--- updates/SportPlan.sql
+++ updates/SportPlan.sql
@@ -1,11 +1,11 @@
 DROP TABLE IF EXISTS sport_plan;
 CREATE TABLE sport_plan(
 id bigint(20) NOT NULL AUTO_INCREMENT,
-user_id bigint(20),
-target_weight bigint(20),
-start_time datetime,
-end_time datetime,
-sport_item_ids varchar(128),
+user_id bigint(20) NOT NULL,
+target_weight bigint(20) NOT NULL,
+start_time datetime NOT NULL,
+end_time datetime NOT NULL,
+sport_item_ids varchar(128) NOT NULL,
 create_time datetime NOT NULL,
 modify_time datetime NOT NULL,
 PRIMARY KEY (id) 
