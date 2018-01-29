83704da40d59962892449903cc11b83127e80de3
lomoye <834033206@qq.com>
2018-01-29 20:44:24 +0800

    fixed

--- updates/SportPlan.sql
+++ updates/SportPlan.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS sport_plan;
 CREATE TABLE sport_plan(
 id bigint(20) NOT NULL AUTO_INCREMENT,
 user_id bigint(20),
+target_weight bigint(20),
 start_time datetime,
 end_time datetime,
 sport_item_ids varchar(128),
